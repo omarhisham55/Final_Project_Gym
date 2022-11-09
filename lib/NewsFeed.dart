ef1de34fdeb95b825031cfb44ea075aeb957489d
omarhisham55 <omarhishamho@gmail.com>
2022-11-09 03:05:17 +0200

    GymHubEdit

--- lib/NewsFeed.dart
+++ lib/NewsFeed.dart
@@ -12,6 +12,95 @@ class Newsfeed extends StatelessWidget {
     required this.panelController
   }) : super(key: key);
 
+  Widget StorySection() =>  Padding(
+    padding: const EdgeInsets.all(15.0),
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Stack(
+          alignment: Alignment.bottomRight,
+          children: [
+            CircleAvatar(
+              radius: 25,
+              backgroundImage: NetworkImage('https://img.search.brave.com/rDyllrwwpvfoJQmRSbGQDH6XvjvF2i9ot7sZp_LPUVU/rs:fit:479:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4x/a0V0a05fNUhES0RF/YTdPUXJTbEd3SGFI/ViZwaWQ9QXBp'),
+            ),
+            CircleAvatar(
+              radius: 10,
+              backgroundColor: Colors.white,
+              child: Center(child: Icon(Icons.add)),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
+
+  Widget NewsSection() =>  Container(
+    color: Color.fromARGB(255, 122, 122, 122),
+    child: Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              CircleAvatar(
+                radius: 20,
+                backgroundImage: NetworkImage('https://img.search.brave.com/rDyllrwwpvfoJQmRSbGQDH6XvjvF2i9ot7sZp_LPUVU/rs:fit:479:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4x/a0V0a05fNUhES0RF/YTdPUXJTbEd3SGFI/ViZwaWQ9QXBp'),
+              ),
+              SizedBox(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Lorem Ipsum',
+                    style: TextStyle(
+                        color: Colors.white
+                    ),
+                  ),
+                  Text(
+                    '12h',
+                    style: TextStyle(
+                        color: Colors.white
+                    ),
+                  ),
+                ],
+              ),
+              new Spacer(),
+              Icon(Icons.height)
+            ],
+          ),
+          // Image.asset('images/bg.png')
+          Padding(
+            padding: const EdgeInsets.symmetric(
+                vertical: 10
+            ),
+            child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    border: Border.all(width: 1)
+                ),
+                child: Image.asset('images/Muscular System 1.png', height: 200,)),
+          ),
+          Row(
+            children: [
+              Icon(Icons.heart_broken_rounded),
+              SizedBox(width: 10),
+              Text(
+                  '1.2k'
+              ),
+              new Spacer(),
+              Text(
+                  '33'
+              ),
+              SizedBox(width: 10),
+              Icon(Icons.chat_bubble,color: Colors.white,),
+            ],
+          )
+        ],
+      ),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) =>
       Container(
@@ -23,102 +112,27 @@ class Newsfeed extends StatelessWidget {
           controller: scrollController,
           children: [
             buildDragButton(),
-            Container(
-              child: Column(
-                children: [
-                  Container(
-                    color: Color.fromARGB(255, 122, 122, 122),
-                    child: Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Stack(
-                            alignment: Alignment.bottomRight,
-                            children: [
-                              CircleAvatar(
-                                radius: 25,
-                                backgroundImage: NetworkImage('https://img.search.brave.com/rDyllrwwpvfoJQmRSbGQDH6XvjvF2i9ot7sZp_LPUVU/rs:fit:479:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4x/a0V0a05fNUhES0RF/YTdPUXJTbEd3SGFI/ViZwaWQ9QXBp'),
-                              ),
-                              CircleAvatar(
-                                radius: 10,
-                                backgroundColor: Colors.white,
-                                child: Center(child: Icon(Icons.add)),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
+            Column(
+              children: [
+                Container(
+                  height: 80,
+                  color: Color.fromARGB(255, 122, 122, 122),
+                  child: ListView.separated(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) => StorySection(),
+                    separatorBuilder: (context, index) => SizedBox(width: 5),
+                    itemCount: 10,
                   ),
-                  SizedBox(height: 10),
-                  Container(
-                    color: Color.fromARGB(255, 122, 122, 122),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              CircleAvatar(
-                                radius: 20,
-                                backgroundImage: NetworkImage('https://img.search.brave.com/rDyllrwwpvfoJQmRSbGQDH6XvjvF2i9ot7sZp_LPUVU/rs:fit:479:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4x/a0V0a05fNUhES0RF/YTdPUXJTbEd3SGFI/ViZwaWQ9QXBp'),
-                              ),
-                              SizedBox(width: 10),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    'Lorem Ipsum',
-                                    style: TextStyle(
-                                      color: Colors.white
-                                    ),
-                                  ),
-                                  Text(
-                                    '12h',
-                                    style: TextStyle(
-                                        color: Colors.white
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              new Spacer(),
-                              Icon(Icons.height)
-                            ],
-                          ),
-                          // Image.asset('images/bg.png')
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                              vertical: 10
-                            ),
-                            child: Container(
-                              width: double.infinity,
-                              decoration: BoxDecoration(
-                                border: Border.all(width: 1)
-                              ),
-                              child: Image.asset('images/Muscular System 1.png', height: 200,)),
-                          ),
-                          Row(
-                            children: [
-                              Icon(Icons.heart_broken_rounded),
-                              SizedBox(width: 10),
-                              Text(
-                                '1.2k'
-                              ),
-                              new Spacer(),
-                              Text(
-                                  '33'
-                              ),
-                              SizedBox(width: 10),
-                              Icon(Icons.chat_bubble,color: Colors.white,),
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              ),
+                ),
+                SizedBox(height: 10),
+                ListView.separated(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) => NewsSection(),
+                    separatorBuilder: (context, index) => SizedBox(height: 10),
+                    itemCount: 15)
+              ],
             )
           ]
         ),
